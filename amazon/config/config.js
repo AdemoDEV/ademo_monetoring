e10d9bce3deccdaf9436fb81a6f8e3558ac41391
AdeMo <58342559+AdemoDEV@users.noreply.github.com>
2025-02-16 16:33:16 +0100

    add config

---
+++ amazon/config/config.js
@@ -0,0 +1,28 @@
+export const Monitor = {
+    EmbedLink : {
+        "dreamland" : {
+            nameSite: "DreamLand",
+            panier: "https://www.dreamland.be/e/fr/ShopCartOverviewCmd?calculationUsageId=-1&catalogId=15501&doConfigurationValidation=Y&updatePrices=1&orderId=.&langId=-2&storeId=13102&errorViewName=ShopCartOverviewCmd&URL=ShopCartOverviewCmd",
+            account: "https://www.dreamland.be/webapp/wcs/stores/servlet/fr/OrderHistoryDisplayView?catalogId=15501&storeId=13102&langId=-2",
+            payment: "https://www.dreamland.be/e/fr/dl/checkout-delivery?",
+        },
+        "auchan" : {
+            nameSite: "Auchan",
+            panier: "https://www.auchan.fr/checkout/cart/",
+            account: "https://www.auchan.fr/client/accueil",
+            payment: "https://www.auchan.fr/checkout/cart",
+        },
+        "amazon" : {
+            nameSite: "Amazon",
+            panier: "https://www.amazon.fr/gp/cart/view.html?ref_=nav_cart",
+            account: "https://www.amazon.fr/gp/css/homepage.html?ref_=nav_youraccount_btn",
+            payment: "https://www.amazon.fr/gp/cart/view.html?ref_=nav_cart",
+        },
+        "micromania" : {
+            nameSite: "Micromania",
+            panier: "https://www.micromania.fr/checkout/cart",
+            account: "https://www.micromania.fr/customer/account",
+            payment: "https://www.micromania.fr/stdcheckout/standard/login",
+        },
+    }
+}
